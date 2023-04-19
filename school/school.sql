7d49554d4f6708916dcf059bff5777628076324b
Janu Sonthi <janu.sonthi@kellogg.northwestern.edu>
2023-04-19 18:47:40 +0000

    school-domain-modeling-4-19

--- school/school.sql
+++ school/school.sql
@@ -15,5 +15,29 @@ CREATE TABLE students (
 );
 
 -- Create the rest of the tables
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY  AUTOINCREMENT,
+  name TEXT, 
+  description TEXT
+);
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY  AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE sections (
+    id INTEGER PRIMARY KEY  AUTOINCREMENT,
+    time TEXT, 
+    course_id INTEGER,
+    teacher_id INTEGER
+);
+--How does a student enroll in courses? We need to design the model
+-- 1. What are the entities invovled? students, courses
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  section_id INTEGER,
+  student_id INTEGER
+);
